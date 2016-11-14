c783df424178141c9532d398354491721f705b3a
Janus2585 <bd.yhome@gmail.com>
2016-11-14 00:32:50 -0600

    Add files via upload

--- views/js/main.js
+++ views/js/main.js
@@ -449,14 +449,14 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    //for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      //var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
+      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    }
+    //var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
     //var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      //document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
-    //}
-    var dx = determineDx(document.querySelectorAll(".randomPizzaContainer"), size);
-    var newwidth = (document.querySelectorAll(".randomPizzaContainer").offsetWidth + dx) + 'px';
-    document.querySelectorAll(".randomPizzaContainer").style.width = newwidth;
+    //document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
   }
 
   changePizzaSizes(size);
